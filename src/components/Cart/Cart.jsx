2bbb581a0beb542a6882189e4ba7a0af9e2573ff
Amin7775 <amin.dpi24@gmail.com>
2023-09-16 12:29:33 +0600

    Added Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,21 @@
 import "./Cart.css"
 
-const Cart = () => {
+const Cart = ({selectedActors,remaining,totalCost}) => {
+    
     return (
         <div>
-            
+            <h3>Total Actors: {selectedActors.length}</h3>
+            <h5>Remaining : {remaining}$</h5>
+            <h5>TotalCost: {totalCost}$</h5>
+          {
+            selectedActors.map(selectedActor =>
+                <div key={selectedActor.id}>
+                    <li>Name:{selectedActor.name} || salary:{selectedActor.salary}</li>
+                   
+
+                </div>
+            )
+          }
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,12 +6,41 @@ import { useState } from "react";
 
 const Home = () => {
   const [allActors, setAllActors] = useState([]);
+    const [selectedActors,setSelectedActors]=useState([]);
+    const [remaining,setRemaining] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
 
   useEffect(() => {
     fetch("./data.json")
       .then((res) => res.json())
       .then((data) => setAllActors(data));
   }, []);
+
+
+  const handleSelectActor= (actor) =>{
+    const isExist = selectedActors.find((item)=> item.id==actor.id);
+
+    let count = actor.salary;
+    if(isExist){
+      return alert('Already booked')
+    }else{
+      selectedActors.forEach(item => {
+        count=count+item.salary;
+      })
+
+      const totalRemaining = 30000-count;
+      if(count>30000){
+        return alert("Out Of Budget")
+      }
+      setTotalCost(count);
+      setRemaining(totalRemaining);
+
+      const newActor=[...selectedActors,actor]
+      setSelectedActors(newActor);
+    }
+  }
+
+  console.log(selectedActors)
 
   return (
     <div className="container">
@@ -35,19 +64,18 @@ const Home = () => {
               </small>
             </p>
             <div className="info">
-              <p>Salary : {actor.salary}</p>
+              <p>Salary : {actor.salary}$</p>
               <p>Role: {actor.role} </p>
             </div>
-            <button className="card-btn">Select</button>
+            <button onClick={()=> handleSelectActor(actor)} className="card-btn">Select</button>
           </div>)
           }
         </div>
         {/* Cart */}
         <div className="cart">
-          <h1>This is cart</h1>
+          <Cart selectedActors={selectedActors} remaining={remaining} totalCost={totalCost}></Cart>
         </div>
       </div>
-      <Cart></Cart>
     </div>
   );
 };
